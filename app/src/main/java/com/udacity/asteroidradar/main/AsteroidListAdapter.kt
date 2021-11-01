c84e0318cba06480a23aa52ac0babbac95d7257a
oscar <oscar.goodwin@gmail.com>
2021-11-01 15:23:18 +0800

    Added RecyclerView

--- app/src/main/java/com/udacity/asteroidradar/main/AsteroidListAdapter.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/AsteroidListAdapter.kt
@@ -1,5 +1,67 @@
 package com.udacity.asteroidradar.main
 
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.udacity.asteroidradar.Asteroid
+import com.udacity.asteroidradar.R
 
-class AsteroidListAdapter
+//RecyclerView class
+class AsteroidListAdapter: ListAdapter<Asteroid, AsteroidListAdapter.ViewHolder>(AsteroidDiffCallback()){
+
+    //Mandatory overrides for ListAdapter
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = getItem(position)
+        holder.bind(item)
+    }
+
+    //Mandatory overrides for ListAdapter
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder.from(parent)
+    }
+
+    //Specific ViewHolder. Can create more of these for different types of data.
+    class ViewHolder private constructor (itemView : View): RecyclerView.ViewHolder(itemView){
+        val asteroidID: TextView = itemView.findViewById(R.id.asteroidIdTextView)
+        val asteroidImpactDate: TextView = itemView.findViewById(R.id.asteroidImpactDateTextView)
+        val asteroidDangerous: ImageView = itemView.findViewById(R.id.asteroidDangerousImage)
+
+        fun bind(item: Asteroid) {
+            val res = itemView.context.resources
+            asteroidID.text = item.id.toString()
+            asteroidImpactDate.text = item.closeApproachDate
+
+            if (item.isPotentiallyHazardous) {
+                asteroidDangerous.setImageResource(R.drawable.ic_status_potentially_hazardous)
+            } else asteroidDangerous.setImageResource(R.drawable.ic_status_normal)
+        }
+
+        //Inflate the template for this specific ViewHolder Type
+        companion object {
+            fun from(parent: ViewGroup): ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val view = layoutInflater.inflate(R.layout.asteroid_list_item, parent, false)
+
+                return ViewHolder(view)
+            }
+        }
+    }
+
+    //Diff util for determining is changes have been made to content, if so then they will be refreshed.
+    class AsteroidDiffCallback : DiffUtil.ItemCallback<Asteroid>() {
+        override fun areItemsTheSame(oldItem: Asteroid, newItem: Asteroid): Boolean {
+            return oldItem.id == newItem.id //Just compares the old and new ID.
+        }
+
+        override fun areContentsTheSame(oldItem: Asteroid, newItem: Asteroid): Boolean {
+            return oldItem == newItem //Compares all data within the item.
+        }
+    }
+}
+
+
--- app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.util.Log
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.udacity.asteroidradar.R
 import com.udacity.asteroidradar.databinding.FragmentMainBinding
 
@@ -18,6 +20,8 @@ class MainFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         val binding = FragmentMainBinding.inflate(inflater)
 
+
+
         // Allows Data Binding to Observe LiveData with the lifecycle of this Fragment
         binding.lifecycleOwner = this
 
@@ -26,7 +30,15 @@ class MainFragment : Fragment() {
 
         setHasOptionsMenu(true)
 
-        //Log.i("MainFragment",viewModel.asteroidList.value.toString())
+        //Setup RecyclerView and observe change to live data from repository.
+        val adapter = AsteroidListAdapter()
+        binding.asteroidRecycler.adapter = adapter //Bind recycler in XML to the ListAdapter [RecyclerView adapter with the ViewHolder]
+        viewModel.asteroidList.observe(viewLifecycleOwner, Observer {
+            it?.let{
+                adapter.submitList(it)
+            }
+
+        })
 
         return binding.root
     }
