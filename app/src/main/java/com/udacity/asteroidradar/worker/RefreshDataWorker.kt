61d4cc512f23793a89e3ac6fe80d2b0d9b40d435
oscar <oscar.goodwin@gmail.com>
2021-11-04 17:23:14 +0800

    Build error:
    RefreshDataWorker.kt: (7, 43): Unresolved reference: getDatabase

---
+++ app/src/main/java/com/udacity/asteroidradar/worker/RefreshDataWorker.kt
@@ -0,0 +1,32 @@
+package com.udacity.asteroidradar.worker
+
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.udacity.asteroidradar.AsteroidsRepository
+import com.udacity.asteroidradar.database.getDatabase
+import retrofit2.HttpException
+
+class RefreshDataWorker(appContext: Context, params: WorkerParameters): CoroutineWorker(appContext, params) {
+
+    companion object{
+        const val WORK_NAME = "RefreshDataWorker"
+    }
+
+
+    override suspend fun doWork(): Result {
+        val database = getDatabase(applicationContext)
+
+        val repository = AsteroidsRepository(database)
+
+        return try {
+            repository.refreshAsteroids()
+            Result.success()
+        } catch (e: HttpException) {
+            Result.retry()
+        }
+
+    }
+
+
+}
